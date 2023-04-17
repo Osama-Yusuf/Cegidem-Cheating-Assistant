117796b0eb80801a02a11f55812f4afd520b07e5
Administrator <osama9mohamed5@gmail.com>
2023-04-17 19:10:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Cegidem Cheating Assistant \
+## with the help of OCR & ChatGpt select the screenshot img from your local machine of the question, and then convert it to a txt then prompt it to ChatGpt and the answer will pop in seconds
+
+# Requirements
+## To use this application, you will need the following:
+
+## An API key for the OCR API from [OCR.space](https://ocr.space/ocrapi).
+## An API key for the OpenAI GPT-3 API. You can obtain a key by signing up for the OpenAI API.
+## A modern web browser with support for HTML5 and JavaScript.
+
+# Getting Started
+## To use this application, follow these steps:
+
+### 1- Clone the repository to your local machine.
+### 2- Replace the YOUR_API_KEY placeholder in the script.js file with your API key for the OCR API.
+### 3- Replace the YOUR_OPENAI_API_KEY placeholder in the script.js file with your API key for the OpenAI GPT-3 API.
+### 4- Open the index.html file in your web browser.
+### 5- Select an image file using the file input field.
+### 6- Click the "Submit" button to extract the text from the image and generate a response using the OpenAI GPT-3 API.
+
+# API Documentation
+## OCR API documentation: [OCR.space](https://ocr.space/ocrapi)
+## OpenAI GPT-3 API documentation: [OpenAI](https://beta.openai.com/docs/api-reference/introduction)
+
+# License
+## This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ script.js
@@ -0,0 +1,78 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const imageInput = document.getElementById("image-input");
+const submitButton = document.getElementById("submit-button");
+const output = document.getElementById("output");
+
+const apiKey = "<Your_OCR_API-KEY_HERE>";
+const configuration = new Configuration({
+    apiKey: "<Your_OpenAI_API-KEY_HERE>",
+});
+
+const openai = new OpenAIApi(configuration);
+
+const ChatGPTFunction = async (text) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: text,
+        temperature: 0.6,
+        max_tokens: 1024,
+        top_p: 1,
+        frequency_penalty: 1,
+        presence_penalty: 1,
+    });
+    return response.data.choices[0].text;
+};
+submitButton.addEventListener("click", function () {
+    if (imageInput.files.length === 0) {
+        alert("Please select an image file!");
+        return;
+    }
+
+    const fileToUpload = imageInput.files[0];
+    const formData = new FormData();
+    formData.append("file", fileToUpload);
+    formData.append("language", "eng");
+    formData.append("apikey", apiKey);
+    formData.append("isOverlayRequired", true);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://api.ocr.space/parse/image");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                const parsedResults = response["ParsedResults"];
+                if (parsedResults !== null) {
+                    let outputText = "";
+                    parsedResults.forEach(function (parsedResult) {
+                        const exitCode = parsedResult["FileParseExitCode"];
+                        const parsedText = parsedResult["ParsedText"];
+                        const errorMessage = parsedResult["ErrorMessage"];
+
+                        if (+exitCode === 1) {
+                            outputText += parsedText + "\n";
+                        } else {
+                            outputText += "Error: " + errorMessage + "\n";
+                        }
+                    });
+                    output.innerText = outputText;
+                } else {
+                    output.innerText = "Error: No parsed results found";
+                }
+            } else {
+                output.innerText = "Error: " + xhr.statusText;
+            }
+        }
+    };
+    xhr.send(formData);
+    // console.log(text)
+    setTimeout(function () {
+        const text = "Answer this in a direct manner: " + output.innerText.replace(/\s+/g, ' ').trim();
+        console.log(text)
+        ChatGPTFunction(text).then((response) => {
+            console.log(response);
+            document.getElementById("output2").textContent = response;
+        });
+    }, 10000); // delay for 10 seconds (10,000 milliseconds)
+});
